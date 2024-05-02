bdb1456ebf7226fbd6c13103334e650de4ec97af
Njugi92 <kenkaruma.kk@gmail.com>
2024-05-02 11:53:58 +0300

    Deploying university website

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        &copy; {new Date().getFullYear()} LKCC University. All rights reserved.
+      </p>
+      <ul>
+        <li>Terms of services</li>
+        <li>Privacy policy</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
